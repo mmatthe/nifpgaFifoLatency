392f4322fec8f07255b1f8bf2c9645a924f71d73
Maximilian Matthe <maximilian.matthe@ifn.et.tu-dresden.de>
2018-09-17 15:36:04 +0200

    add parallel fifo tests, make code more flexible

--- src/latency.cpp
+++ src/latency.cpp
@@ -8,6 +8,7 @@
 #include <thread>
 #include <fstream>
 #include <thread>
+#include <functional>
 
 #include <cstdlib>
 #include <ctime>
@@ -25,8 +26,8 @@
 
 template <class data_type>
 std::vector<uint64_t> measure_latency(NiFpga_Session session,
-				 nifpga::Fifo<data_type>& fifo_in,
-				 nifpga::Fifo<data_type>& fifo_out,
+				 nifpga::Fifo<data_type> fifo_in,
+				 nifpga::Fifo<data_type> fifo_out,
 				 int elements_per_block, int num_tries) {
   using namespace std::chrono_literals;
 
@@ -53,6 +54,35 @@ std::vector<uint64_t> measure_latency(NiFpga_Session session,
   }
   return result;
 }
+
+template <class data_type>
+std::function<std::vector<uint64_t>()> measureFunc(NiFpga_Session session,
+						   nifpga::Fifo<data_type> fifo_in,
+						   nifpga::Fifo<data_type> fifo_out,
+						   int elements_per_block, int num_tries) {
+  return [=]() { return measure_latency(session, fifo_in, fifo_out, elements_per_block, num_tries); };
+}
+
+std::map<std::string, std::vector<uint64_t> > measure_latencies(NiFpga_Session session, int num_runs, int num_elements, int parallelism) {
+
+  std::map<std::string, std::function<std::vector<uint64_t>()>> functions;
+  functions["I32"] = measureFunc(session, fifo_FIFO_I32H2T, fifo_FIFO_I32T2H, num_elements, num_runs);
+  functions["U64"] = measureFunc(session, fifo_FIFO_U64H2T, fifo_FIFO_U64T2H, num_elements, num_runs);
+
+  std::map<std::string, std::vector<uint64_t> > result;
+  std::vector<std::thread> threads;
+  for (auto& kv: functions) {
+    threads.push_back(std::thread([&result, kv]() { result[kv.first] = kv.second(); }));
+    if (threads.size() >= parallelism) {
+      for(auto& t: threads) t.join();
+      threads.clear();
+    }
+  }
+  for(auto& t: threads) t.join();  // Wait for remaining threads  
+  
+  return result;
+}
+
 
 void configureFifos(NiFpga_Session session) {
     const int DEPTH=100000000;
@@ -83,13 +113,6 @@ void testSystem(NiFpga_Session session) {
     std::cout << "Done" << std::endl;
 }
 
-std::map<std::string, std::vector<uint64_t> > measure_latencies(NiFpga_Session session, int num_runs, int num_elements) {
-  std::map<std::string, std::vector<uint64_t> > result;
-
-  result["I32"] = measure_latency(session, fifo_FIFO_I32H2T, fifo_FIFO_I32T2H, num_elements, num_runs);
-  result["U64"] = measure_latency(session, fifo_FIFO_U64H2T, fifo_FIFO_U64T2H, num_elements, num_runs);
-  return result;
-}
 
 int main() {
   std::srand(std::time(nullptr));
@@ -109,7 +132,7 @@ int main() {
     for(auto elems: numElements) {
       std::cout << "Transmitting " << elems << " elements...";
 
-      std::map<std::string, std::vector<uint64_t> > latencies = measure_latencies(session, 100, elems);
+      std::map<std::string, std::vector<uint64_t> > latencies = measure_latencies(session, 100, elems, 4);
 
       for (auto kv: latencies) {
 	std::string fn = "results/ " + kv.first + "_" + std::to_string(elems) + ".txt";
